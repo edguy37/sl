b435591051fe71e1bb0cd5f0733d318762b2d230
Enrique Gamboa <edguy37@gmail.com>
2021-04-13 20:22:44 -0500

    nueva barra de busqueda

--- resources/views/livewire/search-bar.blade.php
+++ resources/views/livewire/search-bar.blade.php
@@ -1,23 +1,23 @@
-<div class="cover" style="background-image: url();">
-    <div  class="cover-wrapper container">
+<div class="cover" style="background-image: url({{ url('storage/imgs/img-buscador.jpg') }});">
+    <div  class="cover-wrapper container mx-auto px-4">
       <div class="logo">
         <a href="#">
-          <img src="#" alt="Inicio"> <!-- Cambiar link por ruta -->
+          <img src="{{ url('storage/imgs/logo.png') }}" alt="Inicio"> <!-- Cambiar link por ruta -->
         </a>
       </div>
       <div class="titulo">
         <p>Nebox te ayuda a encontrar descuentos cerca de ti</p>
       </div>
-      <form class="buscador" id="buscador" action="#" method="get">
+      <form class="buscador" action="#" method="get">
         <div class="busqueda">
           <div class="campo-buscar">
             <input class="txt-buscador" type="text" name="search" autocomplete="off" placeholder="¿Qué estás buscando?" autofocus/>
           </div>
           <div class="campo-ubicacion">
-            <select class="form-control select-ubic" name="ubic" id="ubic">
+            <select class=" px-2 select-ubic" name="ubic" id="ubic">
               <!-- <option value="Cancún">Cancún</option> -->
                <!-- GCP Se agrega otras ciudades ademas de Mérida  -->
-              <option value="" selected="" disabled="">seleccione ubicacion</option>
+              <option value="" selected="" disabled="">selecciona ubicacion</option>
               <option value="Cancún">Cancún</option>
               <option value="Ciudad del Carmen Campeche">Ciudad del Carmen Campeche</option>
               <option value="Isla Mujeres">Isla Mujeres</option>
