b26fd5b8de12dbd6c28711ecf9e697dd86628dde
Enrique Gamboa <edguy37@gmail.com>
2021-04-14 20:29:28 -0500

    new GirlsList component

---
+++ app/Http/Livewire/GirlsList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class GirlsList extends Component
+{
+    public function render()
+    {
+        return view('livewire.girls-list');
+    }
+}
