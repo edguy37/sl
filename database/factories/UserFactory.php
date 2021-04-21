7f13bcb6957edb69f342d7771b1ecc87e882f30d
Enrique Gamboa <edguy37@gmail.com>
2021-04-21 15:13:05 -0500

    New fields added to user factory.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,12 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->phoneNumber(),
+            'whats' => $this->faker->phoneNumber(),
+            'place' => rand(0,1),
+            'twitter' => $this->faker->lexify('???????'),
+            'instagram' => $this->faker->lexify('???????'),
+            'link' => $this->faker->url(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
