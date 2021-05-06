ef36726107cf657cd4704559b1c6b1ad58b0779a
Enrique Gamboa <edguy37@gmail.com>
2021-05-05 20:04:04 -0500

    user hasManyThrough image-publication

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function images()
+    {
+        return $this->hasManyThrough(Image::class, Publication::class);
+    }
 }
