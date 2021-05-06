02a3ae5ac388783aa8809da10f0cc7fd36653893
Enrique Gamboa <edguy37@gmail.com>
2021-05-05 19:59:44 -0500

    Image model -a created  and integrated

---
+++ database/factories/ImageFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Image;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ImageFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Image::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'image' => $this->faker->imageUrl(1280,720),
+            'publication_id' => rand(1,35)
+        ];
+    }
+}
