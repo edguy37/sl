65fc3b2559d8a6b4af1e8901d54d2c1d6d3c31d7
Enrique Gamboa <edguy37@gmail.com>
2021-05-05 20:05:42 -0500

    4 new methods added to publication

--- app/Models/Publication.php
+++ app/Models/Publication.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Publication extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function image()
+    {
+        return $this->hasMany(Image::class);
+    }
+    public function getPricesAttribute()
+    {
+        $p = $this->price;
+        return "$" . $p;
+    }
+    public function getExcerptAttribute() {
+        return substr($this->description, 0, 280) . "...";
+    }
 }
